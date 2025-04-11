afe88820f0d0eafa43577e6bfb02b0465b317a58
Rudrasingh5134 <rudra513420@gmail.com>
2025-04-11 15:29:27 +0530

    rename

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for config project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The urlpatterns list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, include
+from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view( template_name="index.html"), name="home"),
+    path("", include("product.urls"), name="product") ,
+
+
 ]
