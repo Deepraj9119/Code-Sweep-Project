b14d4c177ceeda641750282ab2e46bba31a94245
Deepraj9119 <deeprajsrivastav935@gmail.com>
2025-04-11 15:30:53 +0530

    login

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
      path("", TemplateView.as_view( template_name="index.html"), name="home"),
-     path("signup", TemplateView.as_view( template_name="signup.html"), name="signup"),
+     path("signup/", TemplateView.as_view( template_name="signup.html"), name="signup"),
+     path("login/", TemplateView.as_view( template_name="login.html"), name="login"),
 ]
