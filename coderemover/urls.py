c92a175f9fb036148d2016804b170f292559911f
Deepraj9119 <deeprajsrivastav935@gmail.com>
2025-04-15 10:14:35 +0530

    update

--- coderemover/urls.py
+++ coderemover/urls.py
@@ -17,11 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import UnusedChecker
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path("", TemplateView.as_view( template_name="index.html"), name="home"),
-     path("signup/", TemplateView.as_view( template_name="signup.html"), name="signup"),
-     path("login/", TemplateView.as_view( template_name="login.html"), name="login"),
-     path("code/",include("coderemover.urls"), name="coderemover"),
+    
+     path("unusedchecker/", UnusedChecker, name="unusedchecker")
 ]
